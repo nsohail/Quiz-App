6a0a8d7e08ba71acf6cb8f9f1637cdc07547b7fa
nsohail <nsohail92@gmail.com>
2014-06-05 16:03:47 -0400

    Answer question_V1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //1. click get started--> fade
+//2. function for submit button
 
 
 $(document).ready(function(){
@@ -7,6 +8,17 @@ $('.getStarted').click(function(){
 $('.overlay').fadeOut(1000);
 });
 
-
-
+var answerChoice= $('.answerBtn').val(); //this is the submit value
 });
+
+
+function startQuiz(){
+	var rightAnswer='18 hours a day';
+
+	$('.submitBtn').click(function(){
+	if(answerChoice===rightAnswer){
+		alert('yay!');
+	}
+	});
+}
+
