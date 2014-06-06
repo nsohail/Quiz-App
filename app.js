dd6d7fe7e2b7edd184be6fd18d9a564ce0495c45
nsohail <nsohail92@gmail.com>
2014-06-05 22:10:11 -0400

    update

--- app.js
+++ app.js
@@ -3,22 +3,25 @@
 
 
 $(document).ready(function(){
-
 $('.getStarted').click(function(){
 $('.overlay').fadeOut(1000);
 });
 
-var answerChoice= $('.answerBtn').val(); //this is the submit value
+
 });
 
 
-function startQuiz(){
-	var rightAnswer='18 hours a day';
+$('.answerBtn').click(function(){
+//$(this).removeClass('btn:focus,btn:active').addClass('answerSelected');
+});
 
-	$('.submitBtn').click(function(){
-	if(answerChoice===rightAnswer){
-		alert('yay!');
-	}
-	});
-}
+
+
+
+
+
+
+
+
+
 
