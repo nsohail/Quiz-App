1e969d7bb650a482cfe5733b29fc2a5ba11970d4
nsohail <nsohail92@gmail.com>
2014-06-06 02:02:44 -0400

    Added correct and incorrect

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//1. click get started--> fade
-//2. function for submit button
-
-
 $(document).ready(function(){
 	$('.getStarted').click(function(){
 		$('.overlay').fadeOut(1000);
 	});
 
 	$('.answerBtn').click(function(){
-		$(this).toggleClass('answerSelected');
+		$('.answerSelected').toggleClass('answerSelected');
+		$(this).addClass('answerSelected');
 	});
 
 	
@@ -18,32 +15,32 @@ $(document).ready(function(){
         question: "Safari parks keep animals locked into small enclosures for up to",
         choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
         questionValue: 0,
-        correct: 0,
-        alertText: "18 hours a day"
+        correct: 0 //HOW DOES THIS KNOW WHICH 0?
     }, {
-        question: "Blah 1",
-        choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
+        question: "One main reason animals suffer in zoos is because",
+        choices: ["Miss their families", "Location", "Horrible Food", "Less space"],
         questionValue: 1,
-        correct: 0,
-        alertText: "blah yes"
+        correct: 3
     }, {
-        question: "Blah 2",
-        choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
+        question: "_________ of animals are caught from the wild solely for money and entertainment in aquariums and zoos",
+        choices: ["50%", "70%", "20%", "10%"],
         questionValue: 2,
-        correct: 0,
-        alertText: "blah yes"
+        correct: 1
     }, {
-        question: "Blah 3",
-        choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
+        question: "Orcas and dolphins in captivity usually live _________ years less than their average lifespan in the wild",
+        choices: ["30 years", "5 years", "10 years", "20 years"],
         questionValue: 3,
-        correct: 0,
-        alertText: "blah yes"
+        correct: 0
     }, {
-        question: "Blah 4",
-        choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
+        question: "Limited swimming space, stress, and warmer water conditions for orcas can cause",
+        choices: ["Better performance tricks", "Hunger", "Happiness", "Their find to collapse"],
         questionValue: 4,
-        correct: 0,
-        alertText: "blah yes"
+        correct: 3
+    }, {
+    	   question:"How can you help end the suffering of animals in captivity?",
+    	   choices:["Donate to organizations that help fight animal abuse", "Boycott zoos, aquariums, circuses, and Seaworld", "Educate others about this matter", "All of the above"],
+    	   questionValue: 5,
+    	   correct:3
     }];
 
 
@@ -56,6 +53,33 @@ $(document).ready(function(){
     $('button.choice4').text(questions[i].choices[3]);
 
     
+
+    //submit button
+    $('button.submitBtn').click(function(){
+	    var userAnswer= $('button').find(".answerBtn.answerSelected").text();
+	    console.log(userAnswer);
+	    
+	    if(userAnswer===undefined){
+	    	alert('Must pick an answer!');
+	    }
+
+	    else if(userAnswer===questions[i].correct){
+	    	alert('CORRECT');
+	    }
+
+	    else{
+	    	alert('INCORRECT');
+	    }
+    
+    //to go to next question
+	    i=i + 1;
+	    $('.question').text(questions[i].question);
+	    $('button.choice1').text(questions[i].choices[0]);
+	    $('button.choice2').text(questions[i].choices[1]);
+	    $('button.choice3').text(questions[i].choices[2]);
+	    $('button.choice4').text(questions[i].choices[3]);
+
+    });//submit function ends
 
 });//end document ready
 
