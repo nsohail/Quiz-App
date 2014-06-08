5821c1cb6f3d7e2ea07ba9243413102b4afe149c
nsohail <nsohail92@gmail.com>
2014-06-07 22:40:24 -0400

    Added correct and incorrect

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    $('.overlay-lightbox').hide();
+
 	$('.getStarted').click(function(){
 		$('.overlay').fadeOut(1000);
 	});
@@ -16,37 +19,38 @@ $(document).ready(function(){
         choices: ["18 hours a day", "10 hours a day", "5 hours a day", "Never"],
         questionValue: 0,
         correct: 0, //HOW DOES THIS KNOW WHICH 0?
-        animals: '50% 40px'
+        animals: '50% 30px'
     }, {
         question: "One main reason animals suffer in zoos is because",
         choices: ["Miss their families", "Location", "Horrible Food", "Less space"],
         questionValue: 1,
         correct: 3,
-        animals: '50% -115px'
+        animals: '50% -125px'
     }, {
         question: "_________ of animals are caught from the wild solely for money and entertainment in aquariums and zoos",
         choices: ["50%", "70%", "20%", "10%"],
         questionValue: 2,
         correct: 1,
-        animals: '50% -266px'
+        animals: '50% -280px'
     }, {
         question: "Orcas and dolphins in captivity usually live _________ years less than their average lifespan in the wild",
         choices: ["30 years", "5 years", "10 years", "20 years"],
         questionValue: 3,
         correct: 0,
-        animals: '50% -419px'
+        animals: '50% -438px'
     }, {
         question: "Limited swimming space, stress, and warmer water conditions for orcas can cause",
-        choices: ["Better performance tricks", "Hunger", "Happiness", "Their find to collapse"],
+        choices: ["Better performance tricks", "Hunger", "Happiness", "Their fins to collapse"],
         questionValue: 4,
         correct: 3,
-        animals: '50% -577px'
+        animals: '50% -595px'
     }, {
     	question:"How can you help end the suffering of animals in captivity?",
     	choices:["Donate to organizations that help fight animal abuse", "Boycott zoos, aquariums, circuses, and Seaworld", "Educate others about this matter", "All of the above"],
     	questionValue: 5,
     	correct:3,
-    	animals: '50% -731px'
+    	animals: '50% -750px'
+
     }];
 
 
@@ -55,6 +59,14 @@ $(document).ready(function(){
     $('.question').text(questions[i].question);
 
 
+    var buttons = ['button.choice1','button.choice2','button.choice3','button.choice4'];
+    for (var b in buttons)
+    {
+        var button = $(buttons[b]); //$('button.choice1')
+        button.attr("value", b);
+        button.text(questions[i].choices[b]);
+    }
+/*
     $('button.choice1').attr( "value", "0");
     $('button.choice2').attr( "value", "1");
     $('button.choice3').attr( "value", "2");
@@ -64,6 +76,8 @@ $(document).ready(function(){
     $('button.choice2').text(questions[i].choices[1]);
     $('button.choice3').text(questions[i].choices[2]);
     $('button.choice4').text(questions[i].choices[3]);
+*/
+
     $('.animals').css('background-position',questions[i].animals);
 
     
@@ -73,30 +87,70 @@ $(document).ready(function(){
 
     //submit button
     $('button.submitBtn').click(function(){
-	    var userAnswer= $('button .answerSelected').val();
+	    var userAnswer= $('button.answerSelected').val();
 	    console.log(userAnswer);
 	    
+        //UNDEFINED
 	    if(userAnswer===undefined){
-	    	alert('Must pick an answer!');
+	    	//alert('Must pick an answer!');
+            $('.overlay-lightbox').fadeIn(300);
+            $('.answer-check').text('Must pick an answer!');
+            $('.answer').text('');
+
+            $('.overlay-lightbox').on('click',$('#exit-icon'), function(){
+                $('.overlay-lightbox').fadeOut(400);
+            });
 	    }
 
-	    else if(userAnswer===questions[i].correct){
-	    	alert('CORRECT');
+        //CORRECT ANSWER
+	    else if(userAnswer==questions[i].correct){
+	    	//alert('CORRECT');
 
-        //to go to next question
-        i=i + 1;
-        $('.question').text(questions[i].question);
-    
+            $('.overlay-lightbox').fadeIn(300);
+            $('.overlay-lightbox .box').css('background-color','#dff0d7');
+            $('.answer-check').text('CORRECT!');
+            $('.answer').text('');
 
-        $('button.choice1').text(questions[i].choices[0]);
-        $('button.choice2').text(questions[i].choices[1]);
-        $('button.choice3').text(questions[i].choices[2]);
-        $('button.choice4').text(questions[i].choices[3]);
-        $('.animals').css('background-position',questions[i].animals);
+            $('.overlay-lightbox').on('click',$('#exit-icon'), function(){
+                $('.overlay-lightbox').fadeOut(400);
+            });
+
+
+            
+            //go to next question
+            i=i+1;  //i=i + 1
+            var questionTitle = $('.question').text(questions[i].question);
+        
+            var nextButtons = ['button.choice1','button.choice2','button.choice3','button.choice4'];
+            for(var b in nextButtons){
+                var nextSet= $(nextButtons[b]);
+                nextSet.text(questions[i].choices[b]);
+            }
+
+            $('button').removeClass('answerSelected');
+
+            /*
+            $('button.choice1').text(questions[i].choices[0]);
+            $('button.choice2').text(questions[i].choices[1]);
+            $('button.choice3').text(questions[i].choices[2]);
+            $('button.choice4').text(questions[i].choices[3]);
+            */
+
+            $('.animals').css('background-position',questions[i].animals);
+
 	    }
 
 	    else{
-	    	alert('INCORRECT');
+            //INCORRECT
+	       //alert('INCORRECT '+ userAnswer + ' is not ' + questions[i].correct);
+           $('.overlay-lightbox').fadeIn(300);
+           $('.overlay-lightbox .box').css('background-color','#f2dede');
+           $('.answer-check').text('INCORRECT!');
+           $('.answer').text('Take another guess!');
+
+           $('.overlay-lightbox').on('click',$('#exit-icon'), function(){
+                $('.overlay-lightbox').fadeOut(400);
+           });
 	    }
     
     
